3676fc6a9d52cea06b21f4860f7df1924f60fa3d
Mark Trevor Birss <markbirss@gmail.com>
2026-01-29 10:33:20 +0200

    Update u8x8_d_st7305.c

--- src/clib/u8x8_d_st7305.c
+++ src/clib/u8x8_d_st7305.c
@@ -509,7 +509,7 @@ static const uint8_t u8x8_d_st7305_168x384_flip0_seq[] = {
 
 
 static const uint8_t u8x8_d_st7305_168x384_init_seq[] = {
-    U8X8_START_TRANSFER(),
+        U8X8_START_TRANSFER(),
 
 
     /* Software Reset */
@@ -519,23 +519,23 @@ static const uint8_t u8x8_d_st7305_168x384_init_seq[] = {
 
     /* Settings */
     // 以下在芯片手册基础上，按照显示屏厂家给的示例代码调整
-    U8X8_CAA(0xD6, 0x13, 0x02), // NVM Load Control
+    U8X8_CAA(0xD6, 0x17, 0x02), // NVM Load Control
     U8X8_CA(0xD1, 0x01),        // Booster Enable
-    U8X8_CAA(0xC0, 0x12, 0x0A), // Gate Voltage Setting: VGH=15V, VGL=-10V
+    U8X8_CAA(0xC0, 0x08, 0x02), // Gate Voltage Setting: VGH=15V, VGL=-10V
 
 
-    U8X8_CAAAA(0xC1, 0x3C, 0x3E, 0x3C, 0x3C), // VSHP Setting: VSHP1~4 = 4.8V
-    U8X8_CAAAA(0xC2, 0x23, 0x21, 0x23, 0x23), // VSLP Setting: VSHP1~4 = 0.98V
-    U8X8_CAAAA(0xC4, 0x5A, 0x5C, 0x5A, 0x5A), // VSHN Setting: VSHP1~4 = -3.6V
-    U8X8_CAAAA(0xC5, 0x37, 0x35, 0x37, 0x37), // VSLN Setting: VSHP1~4 = 0.22V
+    U8X8_CAAAA(0xC1, 0X0B, 0X0B, 0X0B, 0X0B), // VSHP Setting: VSHP1~4 = 4.8V
+    U8X8_CAAAA(0xC2, 0x23, 0X23, 0x23, 0x23), // VSLP Setting: VSHP1~4 = 0.98V
+    U8X8_CAAAA(0xC4, 0X27, 0X27, 0X27, 0X27), // VSHN Setting: VSHP1~4 = -3.6V
+    U8X8_CAAAA(0xC5, 0X35, 0x35, 0X35, 0X35), // VSLN Setting: VSHP1~4 = 0.22V
 
 
-    U8X8_CAA(0xD8, 0xA6, 0xE9), // OSC Setting
+    //U8X8_CAA(0xD8, 0xA6, 0xE9), // OSC Setting
     U8X8_CA(0xB2, 0x12),        // Frame Rate Control: HPM=32Hz, LPM=1Hz
 
 
-    U8X8_CAAAA(0xB3, 0xE5, 0xF6, 0x17, 0x77), // Update Period Gate EQ Control in HPM
-    U8X8_A6(0x77, 0x77, 0x77, 0x77, 0x77, 0x71),
+    U8X8_CAAAA(0xB3, 0xE5, 0xF6, 0X05, 0X46), // Update Period Gate EQ Control in HPM
+    U8X8_A6(0x77, 0x77, 0x77, 0x77, 0X76, 0X45),
 
 
     U8X8_CAA(0xB4, 0x05, 0x46), // Update Period Gate EQ Control in LPM
@@ -544,7 +544,7 @@ static const uint8_t u8x8_d_st7305_168x384_init_seq[] = {
 
     U8X8_CAAA(0x62, 0x32, 0x03, 0x1F), // Gate Timing Control
     U8X8_CA(0xB7, 0x13),               // Source EQ Enable
-    U8X8_CA(0xB0, 0x60),               // Gate Line Setting: 384 line
+    U8X8_CA(0xB0, 0X64),               // Gate Line Setting: 384 line
 
 
     U8X8_C(0x11), // Sleep Out
@@ -552,17 +552,17 @@ static const uint8_t u8x8_d_st7305_168x384_init_seq[] = {
 
 
     U8X8_CA(0xC9, 0x00), // Source Voltage Select: VSHP1; VSLP1 ; VSHN1 ; VSLN1
-    U8X8_CA(0x36, 0x48), // Memory Data Access Control
+    U8X8_CA(0x36, 0X00), // Memory Data Access Control
     U8X8_CA(0x3A, 0x11), // Data Format Select
     U8X8_CA(0xB9, 0x20), // Gamma Mode Setting: Mono 00:4GS
     U8X8_CA(0xB8, 0x29), // Panel Setting, 1-Dot inversion, Frame inversion, One Line Interlace
 
 
-    U8X8_CAA(0x2A, 0x17, 0x24), // Column Address Setting: 0X24-0X16=14, 14*12=168
-    U8X8_CAA(0x2B, 0x00, 0xBF), // Row Address Setting: 192*2=384
+    U8X8_CAA(0x2A, 0X12, 0X2A), // Column Address Setting: 0X24-0X16=14, 14*12=168
+    U8X8_CAA(0x2B, 0x00, 0XC7), // Row Address Setting: 192*2=384
     U8X8_CA(0x35, 0x00),        // TE Setting
     U8X8_CA(0xD0, 0xFF),        // Enable Auto Power down
-    U8X8_C(0x38),               // High Power Mode ON
+    U8X8_C(0x39),               // High Power Mode ON
     // U8X8_C(0x39),               // Low Power Mode ON
 
 
@@ -596,12 +596,12 @@ static const u8x8_display_info_t u8x8_st7305_168x384_display_info =
         /* i2c_bus_clock_100kHz = */ 4, /* 400KHz */
         /* data_setup_time_ns = */ 15,
         /* write_pulse_width_ns = */ 70,
-        /* tile_width = */ 21, /* tile width is 21*8=168, because this display requires 12 bit blocks, which would be 168 pixel, so next tile is at 168 */
+        /* tile_width = */ 40, /* tile width is 21*8=168, because this display requires 12 bit blocks, which would be 168 pixel, so next tile is at 168 */
         /* tile_height = */ 48,
         /* default_x_offset = */ 0,
         /* flipmode_x_offset = */ 0,
-        /* pixel_width = */ 168, /* not 100% sure, whether this works with the tile_width of 21... */
-        /* pixel_height = */ 384};
+        /* pixel_width = */ 300, /* not 100% sure, whether this works with the tile_width of 21... */
+        /* pixel_height = */ 400};
 
 
 uint8_t u8x8_d_st7305_168x384(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
